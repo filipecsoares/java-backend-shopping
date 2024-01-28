5ac47d7d4008933d36a46ea2baca47905aa91dc9
filipecsoares <filipecarlossoares@gmail.com>
2024-01-28 17:39:24 -0300

    feat: user-api create user service

---
+++ user-api/src/main/java/com/fcs/userapi/service/UserService.java
@@ -0,0 +1,42 @@
+package com.fcs.userapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.fcs.userapi.model.User;
+import com.fcs.userapi.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserByEmail(final String email) {
+        return userRepository.getUserByEmail(email);
+    }
+
+    public Optional<List<User>> getAll() {
+        return Optional.of(userRepository.findAll());
+    }
+
+    public Optional<List<User>> findByName(final String name) {
+        return userRepository.findByNameLike(name);
+    }
+
+    public Optional<User> findById(final long id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(final User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(final long id) {
+        userRepository.deleteById(id);
+    }
+}
