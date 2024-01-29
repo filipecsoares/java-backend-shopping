dede7d3a6cd3670d61d76922762015e905afa18f
filipecsoares <filipecarlossoares@gmail.com>
2024-01-29 20:33:36 -0300

    feat: productapi create models

---
+++ product-api/src/main/java/com/fcs/productapi/model/Product.java
@@ -0,0 +1,85 @@
+package com.fcs.productapi.model;
+
+import java.util.Locale.Category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Float price;
+    private String description;
+    private String productIdentifier;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Product() {
+        
+    }
+
+    public Product(String name, Float price, String description, String productIdentifier, Category category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.productIdentifier = productIdentifier;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductIdentifier() {
+        return productIdentifier;
+    }
+
+    public void setProductIdentifier(String productIdentifier) {
+        this.productIdentifier = productIdentifier;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
