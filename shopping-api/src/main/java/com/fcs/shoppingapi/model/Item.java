1ebd6b9bc7aabe7688438894f367e20df3743271
filipecsoares <filipecarlossoares@gmail.com>
2024-01-31 20:53:59 -0300

    feat: shopping-api create models

---
+++ shopping-api/src/main/java/com/fcs/shoppingapi/model/Item.java
@@ -0,0 +1,34 @@
+package com.fcs.shoppingapi.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Item {
+    private String productIdentifier;
+    private Float price;
+
+    public Item() {
+
+    }
+
+    public Item(String productIdentifier, Float price) {
+        this.productIdentifier = productIdentifier;
+        this.price = price;
+    }
+
+    public String getProductIdentifier() {
+        return productIdentifier;
+    }
+
+    public void setProductIdentifier(String productIdentifier) {
+        this.productIdentifier = productIdentifier;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
---
+++ shopping-api/src/main/java/com/fcs/shoppingapi/model/Shop.java
@@ -0,0 +1,78 @@
+package com.fcs.shoppingapi.model;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "shop")
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String userIdentifier;
+    private float total;
+    private Date createdAt;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "item", joinColumns = @JoinColumn(name = "shop_id"))
+    private List<Item> items;
+
+    public Shop() {
+        
+    }
+
+    public Shop(long id, String userIdentifier, float total, Date createdAt, List<Item> items) {
+        this.id = id;
+        this.userIdentifier = userIdentifier;
+        this.total = total;
+        this.createdAt = createdAt;
+        this.items = items;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserIdentifier() {
+        return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+        this.userIdentifier = userIdentifier;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
